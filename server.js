2d5e711c4e16074251a4ce3bcaeef70422bc63aa
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-08-28 04:29:14 -0700

    Port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 var routes = require("./controllers/controller.js");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ArticleScraper";
 
 app.use(bodyParser.urlencoded({ extended: true }));
